123663264fd393f36f171ab997382330f27450bd
Somov Studio <somov.studio@gmail.com>
2023-05-14 11:44:29 +0300

    Добавлен отчет некорректных страниц

--- SEOScanner/SEOScanner/Form1.cs
+++ SEOScanner/SEOScanner/Form1.cs
@@ -98,6 +98,12 @@ namespace SEOScanner
             reportRichTextBox.Update();
         }
 
+        private void addReportMessageFailed(string message)
+        {
+            reportFailedRichTextBox.Text = reportFailedRichTextBox.Text + Environment.NewLine + message;
+            reportFailedRichTextBox.Update();
+        }
+
         private const string BLUE = "blue";
         private const string YELLOW = "yellow";
         private const string RED = "red";
@@ -576,6 +582,7 @@ namespace SEOScanner
             toolStripStatusLabelProcessPercent.Text = Convert.ToString(percent) + "%";
             listView1.Items.Clear();
             reportRichTextBox.Text = "";
+            reportFailedRichTextBox.Text = "";
 
             webBrowser1.ScriptErrorsSuppressed = true;
             
@@ -602,6 +609,7 @@ namespace SEOScanner
             string _type_get_value_from;              // получить значение из
             string _get_value_from_attribute_name;    // имя аттрибута
 
+            bool failed = false;
             int amountItems = listView2.Items.Count;
             for (int i = 0; i < amountItems; i++)
             {
@@ -623,18 +631,23 @@ namespace SEOScanner
                     {
                         //addReportMessage(_description + " | Тег: " + _search_by_tag_name + " " + _search_by_tag_id + " | Аттрибут: " + _search_by_tag_attribute + " " + _search_by_tag_attribute_value + " | Тип: " + _type_get_value_from + " " + _get_value_from_attribute_name + " | Значение: " + value);
                         addReportMessage(_description + ": " + value);
-                        if(value != "" && value != null) addItemInTableReport(BLUE, "", _description, value);
-                        else addItemInTableReport(YELLOW, "", _description, "");
+                        if (value != "" && value != null) addItemInTableReport(BLUE, "", _description, value);
+                        else
+                        {
+                            failed = true;
+                            addItemInTableReport(YELLOW, "", _description, "");
+                        }
                     }
                 }
                 else
                 {
+                    failed = true;
                     addReportMessage(_description + ":");
                     addItemInTableReport(YELLOW, "", _description, "");
                 }
             }
             addReportMessage("");
-
+            if (failed == true) addReportMessageFailed(currentURL);
 
             toolStripProgressBar1.Value = step;
             percent = (int)(((double)toolStripProgressBar1.Value / (double)toolStripProgressBar1.Maximum) * 100);
@@ -886,6 +899,30 @@ namespace SEOScanner
         {
             Form3 about = new Form3();
             about.ShowDialog();
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            reportFailedRichTextBox.Clear();
+        }
+
+        private void toolStripButton2_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                saveFileDialog1.FileName = "";
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    reportFailedRichTextBox.SaveFile(saveFileDialog1.FileName, RichTextBoxStreamType.PlainText);
+                    if (File.Exists(saveFileDialog1.FileName)) addConsoleMessage("Отчет успешно сохранён в файл " + saveFileDialog1.FileName);
+                    else addConsoleMessage("Ошибка: Отчет не удалось сохранить в файл " + saveFileDialog1.FileName);
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+                addConsoleMessage("Ошибка: " + error.Message);
+            }
         }
 
         private void radioButton1_CheckedChanged(object sender, EventArgs e)
